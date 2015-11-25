1775b8fa6ee1610c8f487f9a70a3f33e1f7a40b0
Danijel Zecevic <danijel.zecevic@marand.si>
2015-11-25 16:25:42 +0100

    -

--- core/src/com/gurella/engine/graph/movement/LinearVelocityProcessor.java
+++ core/src/com/gurella/engine/graph/movement/LinearVelocityProcessor.java
@@ -5,31 +5,54 @@ import com.badlogic.gdx.math.Vector3;
 import com.gurella.engine.application.UpdateOrder;
 import com.gurella.engine.graph.SceneNode;
 import com.gurella.engine.graph.SceneProcessor;
+import com.gurella.engine.graph.manager.ComponentsPredicate;
 import com.gurella.engine.graph.manager.SceneNodeManager;
+import com.gurella.engine.graph.manager.SceneNodeManager.SceneNodeFamily;
 import com.gurella.engine.signal.Listener0;
 
 public class LinearVelocityProcessor extends SceneProcessor {
+	private static final SceneNodeFamily family = new SceneNodeFamily(
+			ComponentsPredicate.all(true, TransformComponent.class, LinearVelocityComponent.class).build());
+
 	private SceneNodeManager nodeManager;
-	@SuppressWarnings("unchecked")
-	private NodeGroup nodeGroup = new ComponentBitsNodeGroup(TransformComponent.class, LinearVelocityComponent.class);
 	private Vector3 tempTranslate = new Vector3();
 	private Vector3 tempVelocity = new Vector3();
-	
+
 	public LinearVelocityProcessor() {
 		activatedSignal.addListener(new ActivateListener());
 		deactivatedSignal.addListener(new DeactivateListener());
 	}
 
 	@Override
+	protected void activated() {
+		nodeManager = getGraph().nodeManager;
+		nodeManager.registerFamily(family);
+	}
+
+	@Override
+	protected void deactivated() {
+		nodeManager.unregisterFamily(family);
+		nodeManager = null;
+	}
+
+	@Override
 	public void update() {
 		float deltaTime = Gdx.graphics.getDeltaTime();
-		
-		for(SceneNode node : nodeManager.getNodes(nodeGroup)) {
-			TransformComponent transformComponent = node.getComponent(TransformComponent.class);
+
+		for (SceneNode node : nodeManager.getNodes(family)) {
 			LinearVelocityComponent linearVelocityComponent = node.getComponent(LinearVelocityComponent.class);
+			TransformComponent transformComponent = node.getComponent(TransformComponent.class);
 			transformComponent.getTranslation(tempTranslate);
-			tempVelocity.set(tempTranslate).sub(linearVelocityComponent.lastPosition);
-			linearVelocityComponent.velocity.set(tempVelocity.x / deltaTime, tempVelocity.y / deltaTime, tempVelocity.z / deltaTime);
+
+			if (linearVelocityComponent.lastPosition.x == Float.NaN) {
+				tempVelocity.setZero();
+			} else {
+
+				tempVelocity.set(tempTranslate).sub(linearVelocityComponent.lastPosition);
+			}
+
+			linearVelocityComponent.velocity.set(tempVelocity.x / deltaTime, tempVelocity.y / deltaTime,
+					tempVelocity.z / deltaTime);
 			linearVelocityComponent.lastPosition.set(tempTranslate);
 		}
 	}
@@ -38,15 +61,14 @@ public class LinearVelocityProcessor extends SceneProcessor {
 	public int getOrdinal() {
 		return UpdateOrder.THINK;
 	}
-	
+
 	private class ActivateListener implements Listener0 {
 		@Override
 		public void handle() {
-			nodeManager = getGraph().nodeManager;
-			nodeManager.registerNodeGroup(nodeGroup);
+
 		}
 	}
-	
+
 	private class DeactivateListener implements Listener0 {
 		@Override
 		public void handle() {
