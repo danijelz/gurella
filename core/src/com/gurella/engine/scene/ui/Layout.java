7e5f40b5f9a898c2a2079f3731e6b0e53e7c8d81
dzn <dzngit@gmail.com>
2015-12-15 11:55:20 +0100

    -

--- core/src/com/gurella/engine/scene/ui/Layout.java
+++ core/src/com/gurella/engine/scene/ui/Layout.java
@@ -1,5 +1,9 @@
 package com.gurella.engine.scene.ui;
 
-public interface Layout {
+import com.badlogic.gdx.math.Vector2;
 
+public interface Layout {
+	Vector2 computeSize(Composite composite, int wHint, int hHint, boolean flushCache);
+
+	void layout(Composite composite, boolean flushCache);
 }
--- core/src/com/gurella/engine/scene/ui/Ui.java
+++ core/src/com/gurella/engine/scene/ui/Ui.java
@@ -2,4 +2,5 @@ package com.gurella.engine.scene.ui;
 
 //TODO unused
 public class Ui extends Composite {
+	private FocusManager focusManager = new FocusManager();
 }
