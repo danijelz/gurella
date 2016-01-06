0ee1b57785f7f0a00bbe44537530227095020443
Dzn <dzngit@gmail.com>
2016-01-06 16:16:23 +0100

    -

--- core/src/com/gurella/engine/base/registry/Prefab.java
+++ core/src/com/gurella/engine/base/registry/Prefab.java
@@ -1,6 +1,8 @@
 package com.gurella.engine.base.registry;
 
 import com.badlogic.gdx.utils.Array;
+import com.gurella.engine.base.model.Model;
+import com.gurella.engine.base.model.Models;
 import com.gurella.engine.base.serialization.ReferenceProperty;
 
 //TODO unused
@@ -9,6 +11,6 @@ public class Prefab {
 	private final Array<ReferenceProperty<?>> referenceProperties = new Array<ReferenceProperty<?>>();
 
 	public ManagedObject instantiate() {
-		
+		Model<ManagedObject> model = Models.getModel(object.getClass());
 	}
 }
