c022e5772467510bcb4d8972b00873272f424de6
dzn <dzngit@gmail.com>
2016-01-11 16:50:26 +0100

    -

--- core/src/com/gurella/engine/base/serialization/ObjectArchive.java
+++ core/src/com/gurella/engine/base/serialization/ObjectArchive.java
@@ -1,24 +1,19 @@
 package com.gurella.engine.base.serialization;
 
 import java.io.IOException;
+import java.io.StringWriter;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.IntMap;
 import com.badlogic.gdx.utils.Json;
-import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.SerializationException;
-import com.badlogic.gdx.utils.reflect.ArrayReflection;
 import com.gurella.engine.asset.Assets;
 import com.gurella.engine.base.model.Model;
 import com.gurella.engine.base.model.Models;
-import com.gurella.engine.base.model.Property;
 import com.gurella.engine.base.registry.ManagedObject;
 import com.gurella.engine.base.resource.AsyncCallback;
 import com.gurella.engine.base.resource.ResourceService;
-import com.gurella.engine.utils.ImmutableArray;
-import com.gurella.engine.utils.SynchronizedPools;
 
 public class ObjectArchive {
 	AsyncCallback<Object> callback;
@@ -26,7 +21,7 @@ public class ObjectArchive {
 	Object rootObject;
 	Class<?> knownType;
 
-	Json json;
+	Json json = new Json();
 	JsonValue serializedObject;
 
 	private Array<AssetReference> dependentAssets = new Array<AssetReference>();
@@ -34,24 +29,28 @@ public class ObjectArchive {
 	private IntMap<ManagedObject> objects = new IntMap<ManagedObject>();
 	private Array<ManagedObject> slicedObjects = new Array<ManagedObject>();
 
-	public void deserialize() {
-		JsonReader reader = SynchronizedPools.obtain(JsonReader.class);
-		serializedObject = reader.parse(Gdx.files.internal(fileName));
-		SynchronizedPools.free(reader);
-		callback.onProgress(0.1f);
-		resolveDependencies();
-	}
-
 	public <T> void serialize(T rootObject, Class<T> knownType) {
-		this.rootObject = rootObject;
-		this.knownType = knownType;
-		serialize();
+		StringWriter buffer = new StringWriter();
+		json.setWriter(buffer);
+		Model<T> model = Models.getModel(rootObject);
+		model.serialize(rootObject, knownType, this);
+		System.out.print(buffer.toString());
+	}
+	
+	public static void main(String[] args) {
+		new ObjectArchive().serialize(new Test(), Test.class);
+	}
+	
+	private static class Test {
+		public int i = 0;
+		public String s = "ddd";
+		public Object[] a = new Object[]{"sss"};
 	}
 
 	private void serialize() {
 		// TODO Auto-generated method stub
 	}
-	
+
 	public void writeObjectStart(Object value, Class<?> knownType) {
 		json.writeObjectStart();
 		Class<? extends Object> actualType = value.getClass();
@@ -83,8 +82,8 @@ public class ObjectArchive {
 
 		} else if (Assets.isAsset(value)) {
 			AssetReference reference = new AssetReference(ResourceService.getResourceFileName(value), value.getClass());
-			if(Assets.isAssetType(knownType)) {
-				
+			if (Assets.isAssetType(knownType)) {
+
 			}
 		} else {
 			Model<Object> model = Models.getModel(value);
@@ -96,7 +95,7 @@ public class ObjectArchive {
 			model.serialize(value, knownType, this);
 		}
 	}
-	
+
 	public void writeValue(Object value, Class<?> knownType) {
 		if (value == null || Serialization.isSimpleType(value)) {
 			json.writeValue(value, knownType);
