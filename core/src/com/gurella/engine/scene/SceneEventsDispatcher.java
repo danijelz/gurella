d2d46079fddeed3ef92dfb69fdea2ce781420972
dzn <dzngit@gmail.com>
2016-03-03 15:55:12 +0100

    -

--- core/src/com/gurella/engine/scene/SceneEventsDispatcher.java
+++ core/src/com/gurella/engine/scene/SceneEventsDispatcher.java
@@ -15,19 +15,20 @@ import com.gurella.engine.subscriptions.scene.SceneActivityListener;
 import com.gurella.engine.utils.Values;
 
 class SceneEventsDispatcher implements ApplicationUpdateListener {
-	private Scene scene;
-	private int instanceId;
+	private final Scene scene;
 	private final Array<Object> tempListeners = new Array<Object>(64);
+
+	private int sceneId;
 
 	SceneEventsDispatcher(Scene scene) {
 		this.scene = scene;
 	}
 
 	void activate() {
-		instanceId = scene.getInstanceId();
+		sceneId = scene.getInstanceId();
 		EventService.subscribe(this);
 		Array<SceneActivityListener> listeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, SceneActivityListener.class, listeners);
+		EventService.getSubscribers(sceneId, SceneActivityListener.class, listeners);
 		for (int i = 0; i < listeners.size; i++) {
 			listeners.get(i).sceneStarted();
 		}
@@ -35,7 +36,7 @@ class SceneEventsDispatcher implements ApplicationUpdateListener {
 
 	void deactivate() {
 		Array<SceneActivityListener> listeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, SceneActivityListener.class, listeners);
+		EventService.getSubscribers(sceneId, SceneActivityListener.class, listeners);
 		for (int i = 0; i < listeners.size; i++) {
 			listeners.get(i).sceneStopped();
 		}
@@ -45,49 +46,49 @@ class SceneEventsDispatcher implements ApplicationUpdateListener {
 	@Override
 	public void update() {
 		Array<IoUpdateListener> ioUpdateListeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, IoUpdateListener.class, ioUpdateListeners);
+		EventService.getSubscribers(sceneId, IoUpdateListener.class, ioUpdateListeners);
 		for (int i = 0; i < ioUpdateListeners.size; i++) {
 			ioUpdateListeners.get(i).onIoUpdate();
 		}
 
 		Array<InputUpdateListener> inputUpdateListeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, InputUpdateListener.class, inputUpdateListeners);
+		EventService.getSubscribers(sceneId, InputUpdateListener.class, inputUpdateListeners);
 		for (int i = 0; i < inputUpdateListeners.size; i++) {
 			inputUpdateListeners.get(i).onInputUpdate();
 		}
 
 		Array<LogicUpdateListener> logicUpdateListeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, LogicUpdateListener.class, logicUpdateListeners);
+		EventService.getSubscribers(sceneId, LogicUpdateListener.class, logicUpdateListeners);
 		for (int i = 0; i < logicUpdateListeners.size; i++) {
 			logicUpdateListeners.get(i).onLogicUpdate();
 		}
 
 		Array<PhysicsUpdateListener> physicsUpdateListeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, PhysicsUpdateListener.class, physicsUpdateListeners);
+		EventService.getSubscribers(sceneId, PhysicsUpdateListener.class, physicsUpdateListeners);
 		for (int i = 0; i < physicsUpdateListeners.size; i++) {
 			physicsUpdateListeners.get(i).onPhisycsUpdate();
 		}
 
 		Array<PreRenderUpdateListener> preRenderUpdateListeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, PreRenderUpdateListener.class, preRenderUpdateListeners);
+		EventService.getSubscribers(sceneId, PreRenderUpdateListener.class, preRenderUpdateListeners);
 		for (int i = 0; i < preRenderUpdateListeners.size; i++) {
 			preRenderUpdateListeners.get(i).onPreRenderUpdate();
 		}
 
 		Array<RenderUpdateListener> renderUpdateListeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, RenderUpdateListener.class, renderUpdateListeners);
+		EventService.getSubscribers(sceneId, RenderUpdateListener.class, renderUpdateListeners);
 		for (int i = 0; i < renderUpdateListeners.size; i++) {
 			renderUpdateListeners.get(i).onRenderUpdate();
 		}
 
 		Array<DebugRenderUpdateListener> debugRenderUpdateListeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, DebugRenderUpdateListener.class, debugRenderUpdateListeners);
+		EventService.getSubscribers(sceneId, DebugRenderUpdateListener.class, debugRenderUpdateListeners);
 		for (int i = 0; i < debugRenderUpdateListeners.size; i++) {
 			debugRenderUpdateListeners.get(i).onDebugRenderUpdate();
 		}
 
 		Array<CleanupUpdateListener> cleanupUpdateListeners = Values.cast(tempListeners);
-		EventService.getSubscribers(instanceId, CleanupUpdateListener.class, cleanupUpdateListeners);
+		EventService.getSubscribers(sceneId, CleanupUpdateListener.class, cleanupUpdateListeners);
 		for (int i = 0; i < cleanupUpdateListeners.size; i++) {
 			cleanupUpdateListeners.get(i).onCleanupUpdate();
 		}
