1f621745a6d1b4263403c17f79a257e90ebec248
dzn <dzngit@gmail.com>
2016-03-17 15:18:09 +0100

    -

--- core/src/com/gurella/engine/scene/bullet/CollisionListener.java
+++
@@ -1,9 +0,0 @@
-package com.gurella.engine.scene.bullet;
-
-public interface CollisionListener {
-	void onCollisionEnter(Collision collision);
-
-	void onCollisionStay(Collision collision);
-
-	void onCollisionExit(BulletPhysicsRigidBodyComponent colidedWith);
-}
