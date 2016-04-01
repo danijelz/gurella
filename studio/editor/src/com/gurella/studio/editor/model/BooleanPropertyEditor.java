9c12ee6f3bd9b1cb45c9df12586d4395be362cce
dzn <dzngit@gmail.com>
2016-04-01 14:33:13 +0200

    -

--- studio/editor/src/com/gurella/studio/editor/model/BooleanPropertyEditor.java
+++ studio/editor/src/com/gurella/studio/editor/model/BooleanPropertyEditor.java
@@ -3,6 +3,7 @@ package com.gurella.studio.editor.model;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -23,6 +24,7 @@ public class BooleanPropertyEditor extends SimplePropertyEditor<Boolean> {
 		layout.marginHeight = 0;
 		setLayout(layout);
 		check = getToolkit().createButton(this, "", SWT.CHECK);
+		check.setBackground(getGurellaEditor().getResourceManager().createColor(new RGB(240, 240, 240)));
 		check.setLayoutData(new GridData(SWT.BEGINNING, SWT.BEGINNING, true, false));
 		check.addSelectionListener(new SelectionAdapter() {
 			@Override
