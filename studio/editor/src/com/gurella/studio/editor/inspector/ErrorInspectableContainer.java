0b1177ba80b8ba89bafce41803f7600a33b148f1
Dzn <dzngit@gmail.com>
2016-04-13 19:41:23 +0200

    -

--- studio/editor/src/com/gurella/studio/editor/inspector/ErrorInspectableContainer.java
+++ studio/editor/src/com/gurella/studio/editor/inspector/ErrorInspectableContainer.java
@@ -23,16 +23,24 @@ public class ErrorInspectableContainer extends InspectableContainer<Throwable> {
 
 		Composite body = getBody();
 		body.setLayout(new GridLayout(1, false));
-		
+
+		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
 		StringWriter writer = new StringWriter();
 		throwable.printStackTrace(new PrintWriter(writer));
-		Text text = toolkit.createText(getBody(), writer.toString(), SWT.H_SCROLL | SWT.V_SCROLL | SWT.MULTI | SWT.READ_ONLY);
-		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		Text text = toolkit.createText(getBody(), writer.toString(),
+				SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.MULTI | SWT.READ_ONLY);
 		text.setLayoutData(layoutData);
-		pack();
+		parent.addListener(SWT.Resize, e -> setTextSize(text, layoutData));
 
 		/*ErrorComposite errorComposite = new ErrorComposite(body, throwable);
 		errorComposite.setLayoutData(layoutData);
 		toolkit.adapt(errorComposite);*/
+	}
+
+	private void setTextSize(Text text, GridData layoutData) {
+		/*layoutData.widthHint = getBody().getSize().x;
+		layoutData.heightHint = getBody().getSize().y;
+		getBody().setSize(getBody().getSize().x - 5, getBody().getSize().y - 5);*/
+		//reflow(true);
 	}
 }
