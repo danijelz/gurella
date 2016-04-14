0973005e8101b46dff395357ed3453b291958435
dzn <dzngit@gmail.com>
2016-04-14 13:50:51 +0200

    -

--- studio/editor/src/com/gurella/studio/editor/inspector/ModelInspectableContainer.java
+++ studio/editor/src/com/gurella/studio/editor/inspector/ModelInspectableContainer.java
@@ -3,6 +3,7 @@ package com.gurella.studio.editor.inspector;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -43,7 +44,9 @@ public class ModelInspectableContainer extends InspectableContainer<IFile> {
 	private CameraInputController camController;
 	private ModelBatch modelBatch;
 	private Environment environment;
+	private Model model;
 	private ModelInstance modelInstance;
+	private Color backgroundColor;
 
 	public ModelInspectableContainer(InspectorView parent, IFile target) {
 		super(parent, target);
@@ -52,11 +55,46 @@ public class ModelInspectableContainer extends InspectableContainer<IFile> {
 		FormToolkit toolkit = GurellaStudioPlugin.getToolkit();
 		toolkit.adapt(this);
 
+		GLData glData = new GLData();
+		glData.redSize = 8;
+		glData.greenSize = 8;
+		glData.blueSize = 8;
+		glData.alphaSize = 8;
+		glData.depthSize = 16;
+		glData.stencilSize = 0;
+		glData.samples = 0;
+		glData.doubleBuffer = false;
+
+		glCanvas = new GLCanvas(body, SWT.FLAT, glData);
+		glCanvas.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true));
+		backgroundColor = getDisplay().getSystemColor(SWT.COLOR_DARK_GRAY);
+		glCanvas.setBackground(backgroundColor);
+
+		Point size = glCanvas.getSize();
+		cam = new PerspectiveCamera(67, size.x, size.y);
+		cam.position.set(1f, 1f, 1f);
+		cam.lookAt(0, 0, 0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		glCanvas.addListener(SWT.Resize, e -> updateSizeByParent());
+
+		camController = new CameraInputController(cam);
+		input = new SwtLwjglInput(glCanvas);
+		input.setInputProcessor(camController);
+
+		modelBatch = new ModelBatch();
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.6f, 0.6f, 0.6f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+		glCanvas.setCurrent();
 		FolderRelativeFileHandleResolver resolver = new FolderRelativeFileHandleResolver((IFolder) target.getParent());
 		ModelLoader<?> loader = new ObjLoader(resolver);
 		FileHandle fileHandle = new FileHandle(target.getLocation().toFile());
 		ModelData modelData = loader.loadModelData(fileHandle);
-		final Model model = new Model(modelData, new TextureProvider() {
+		model = new Model(modelData, new TextureProvider() {
 			@Override
 			public Texture load(String fileName) {
 				Texture result = new Texture(new FileHandle(fileName));
@@ -67,39 +105,14 @@ public class ModelInspectableContainer extends InspectableContainer<IFile> {
 		});
 		modelInstance = new ModelInstance(model);
 
-		GLData glData = new GLData();
-		glData.redSize = 8;
-		glData.greenSize = 8;
-		glData.blueSize = 8;
-		glData.alphaSize = 8;
-		glData.depthSize = 16;
-		glData.stencilSize = 0;
-		glData.samples = 0;
-		glData.doubleBuffer = false; // TODO
-		
-		glCanvas = new GLCanvas(body, SWT.FLAT, glData);
-		glCanvas.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true));
-		
-		Point size = glCanvas.getSize();
-		cam = new PerspectiveCamera(67, size.x, size.y);
-		cam.position.set(1f, 1f, 1f);
-		cam.lookAt(0, 0, 0);
-		cam.near = 1f;
-		cam.far = 300f;
-		cam.update();
-		
-		glCanvas.addListener(SWT.Resize, e -> updateSizeByParent());
-		
-		camController = new CameraInputController(cam);
-		input = new SwtLwjglInput(glCanvas);
-		input.setInputProcessor(camController);
-
-		modelBatch = new ModelBatch();
-		environment = new Environment();
-		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
-		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
-
+		addDisposeListener(e -> onDispose());
 		render();
+	}
+
+	private void onDispose() {
+		glCanvas.dispose();
+		model.dispose();
+		modelBatch.dispose();
 	}
 
 	private void updateSizeByParent() {
@@ -119,9 +132,10 @@ public class ModelInspectableContainer extends InspectableContainer<IFile> {
 		glCanvas.setCurrent();
 
 		Point size = glCanvas.getSize();
-		gl20.glClearColor(1, 1, 1, 1);
-		gl20.glViewport(0, 0, size.x, size.y);
+		Color color = backgroundColor;
+		gl20.glClearColor(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, color.getAlpha() / 255f);
 		gl20.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		gl20.glViewport(0, 0, size.x, size.y);
 
 		modelBatch.begin(cam);
 		modelBatch.render(modelInstance, environment);
