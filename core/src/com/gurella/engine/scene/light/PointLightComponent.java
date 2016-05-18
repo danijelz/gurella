70a46cbbda90fcc48b2c591b29d07ffc5eac542e
Dzn <dzngit@gmail.com>
2016-05-18 19:59:15 +0200

    -

--- core/src/com/gurella/engine/scene/light/PointLightComponent.java
+++ core/src/com/gurella/engine/scene/light/PointLightComponent.java
@@ -2,10 +2,19 @@ package com.gurella.engine.scene.light;
 
 import com.badlogic.gdx.graphics.g3d.environment.PointLight;
 import com.badlogic.gdx.math.Vector3;
+import com.gurella.engine.scene.SceneNodeComponent2;
+import com.gurella.engine.scene.transform.TransformComponent;
+import com.gurella.engine.subscriptions.scene.NodeComponentActivityListener;
+import com.gurella.engine.subscriptions.scene.movement.NodeTransformChangedListener;
+import com.gurella.engine.subscriptions.scene.update.PreRenderUpdateListener;
 
-public class PointLightComponent extends LightComponent<PointLight> {
+public class PointLightComponent extends LightComponent<PointLight>
+		implements NodeComponentActivityListener, NodeTransformChangedListener, PreRenderUpdateListener {
 	private final Vector3 position = new Vector3();
 	private float intensity = 0.1f;
+
+	private transient TransformComponent transformComponent;
+	private transient boolean dirty = true;
 
 	@Override
 	protected PointLight createLight() {
@@ -30,5 +39,48 @@ public class PointLightComponent extends LightComponent<PointLight> {
 	public void setPosition(Vector3 position) {
 		this.position.set(position);
 		light.position.set(position);
+	}
+
+	@Override
+	protected void onActivate() {
+		transformComponent = getNode().getActiveComponent(TransformComponent.class);
+	}
+
+	@Override
+	protected void onDeactivate() {
+		transformComponent = null;
+		dirty = true;
+	}
+
+	@Override
+	public void nodeComponentActivated(SceneNodeComponent2 component) {
+		if (component instanceof TransformComponent) {
+			this.transformComponent = (TransformComponent) component;
+			dirty = true;
+		}
+	}
+
+	@Override
+	public void nodeComponentDeactivated(SceneNodeComponent2 component) {
+		if (component instanceof TransformComponent) {
+			transformComponent = null;
+			dirty = true;
+		}
+	}
+
+	@Override
+	public void onNodeTransformChanged() {
+		dirty = true;
+	}
+
+	@Override
+	public void onPreRenderUpdate() {
+		if (dirty) {
+			if (transformComponent == null) {
+				light.position.setZero().add(position);
+			} else {
+				transformComponent.getWorldTranslation(light.position).add(position);
+			}
+		}
 	}
 }
