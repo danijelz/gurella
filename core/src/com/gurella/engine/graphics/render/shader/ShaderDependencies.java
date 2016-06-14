ddd3bcf7c4c5265c11953dce3db425b02551c3cb
dzn <dzngit@gmail.com>
2016-06-14 13:05:30 +0200

    -

--- core/src/com/gurella/engine/graphics/render/shader/ShaderDependencies.java
+++
@@ -1,7 +0,0 @@
-package com.gurella.engine.graphics.render.shader;
-
-import com.badlogic.gdx.files.FileHandle;
-
-public class ShaderDependencies {
-	private FileHandle shaderPath;
-}
--- core/src/com/gurella/engine/graphics/render/shader/ShaderTemplate.java
+++ core/src/com/gurella/engine/graphics/render/shader/ShaderTemplate.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.IdentityMap;
 
 public class ShaderTemplate {
 	//private static Pattern includePattern = Pattern.compile("^#include\\s+?<[^\\n\\r\\f]+?>\\s+?$");
@@ -12,7 +12,8 @@ public class ShaderTemplate {
 	
 	private final FileHandle path;
 	private final StringBuilder raw;
-	private final ObjectMap<ShaderDependency, ShaderTemplate> dependencies = new ObjectMap<ShaderDependency, ShaderTemplate>();
+	private final IdentityMap<FileHandle, ShaderTemplate> dependencies = new IdentityMap<FileHandle, ShaderTemplate>();
+	private final IdentityMap<ShaderDependencyBlock, FileHandle> dependencyBlocks = new IdentityMap<ShaderDependencyBlock, FileHandle>();
 
 	public ShaderTemplate(FileHandle path) {
 		this.path = path;
@@ -23,22 +24,23 @@ public class ShaderTemplate {
 	private void extractIncludes() {
 		Matcher matcher = includePattern.matcher(raw);
 		while(matcher.find()) {
-			int start = matcher.start();
-			int end = matcher.end();
-			String group = matcher.group(2);
+			ShaderDependencyBlock block = new ShaderDependencyBlock();
+			block.start = matcher.start();
+			block.end = matcher.end();
+			block.path = matcher.group(2);
 		}
 		// TODO Auto-generated method stub
 		
 	}
 
-	private static class ShaderDependency {
+	private static class ShaderDependencyBlock {
 		private int start;
 		private int end;
-		private String dependencyPath;
+		private String path;
 	}
 	
 	public static void main(String[] args) {
-		String raw = "ddd\n\r" + 
+		String raw = "ddd\n\r//" + 
 				"#include <ddd/aaa/nnn1>\n";
 		raw  = "ddd//\n" + 
 				"#include <ddd/aaa/nnn1> /#include <ddd/aaa/nnn2>//\n" + 
