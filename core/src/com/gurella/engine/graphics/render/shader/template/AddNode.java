1aab6666fb1dd83599956e82f74aaf05bf834a68
Dzn <dzngit@gmail.com>
2016-06-30 20:38:14 +0200

    -

---
+++ core/src/com/gurella/engine/graphics/render/shader/template/AddNode.java
@@ -0,0 +1,28 @@
+package com.gurella.engine.graphics.render.shader.template;
+
+import com.gurella.engine.graphics.render.shader.generator.ShaderGeneratorContext;
+
+public class AddNode extends ShaderTemplateNode {
+	private String varName;
+	private int varValue;
+
+	public AddNode(String value) {
+		String[] params = value.split(",");
+		varName = params[0].trim();
+		try {
+			varValue = Integer.parseInt(params[1].trim());
+		} catch (Exception e) {
+			varValue = 0;
+		}
+	}
+
+	@Override
+	protected String toStringValue() {
+		return varName + ", " + varValue;
+	}
+
+	@Override
+	protected void generate(ShaderGeneratorContext context) {
+		context.setValue(varName, context.getValue(varName) + varValue);
+	}
+}
