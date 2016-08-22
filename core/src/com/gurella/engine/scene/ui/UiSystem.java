a427695b25701a0ddd76ccbb52d0df571ff95705
Dzn <dzngit@gmail.com>
2016-08-22 22:09:50 +0200

    -

--- core/src/com/gurella/engine/scene/ui/UiSystem.java
+++ core/src/com/gurella/engine/scene/ui/UiSystem.java
@@ -1,7 +1,37 @@
 package com.gurella.engine.scene.ui;
 
+import com.gurella.engine.base.object.ManagedObject;
+import com.gurella.engine.scene.SceneNodeComponent2;
 import com.gurella.engine.scene.SceneService;
+import com.gurella.engine.subscriptions.base.object.ObjectsParentListener;
+import com.gurella.engine.subscriptions.scene.ComponentActivityListener;
 
-public class UiSystem extends SceneService {
+public class UiSystem extends SceneService implements ComponentActivityListener, ObjectsParentListener {
 	private FocusManager focusManager = new FocusManager();
+
+	@Override
+	public void componentActivated(SceneNodeComponent2 component) {
+		if (component instanceof UiComponent) {
+
+		}
+	}
+
+	@Override
+	public void componentDeactivated(SceneNodeComponent2 component) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void parentChanged(ManagedObject object, ManagedObject oldParent, ManagedObject newParent) {
+		if (object instanceof UiComponent) {
+			UiComponent component = (UiComponent) object;
+			component.parent = findNewParentComposite(newParent);
+		}
+	}
+
+	private Composite findNewParentComposite(ManagedObject newParent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
