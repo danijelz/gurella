dac34ac7fa3bf2e9edd1fcca571766362f9775d6
Dzn <dzngit@gmail.com>
2016-08-28 22:58:16 +0200

    -

---
+++ core/src/com/gurella/engine/editor/ui/EditorTable.java
@@ -0,0 +1,109 @@
+package com.gurella.engine.editor.ui;
+
+public interface EditorTable extends EditorComposite {
+	void clear(int index);
+
+	void clear(int[] indices);
+
+	void clear(int start, int end);
+
+	void clearAll();
+
+	void deselect(int index);
+
+	void deselect(int[] indices);
+
+	void deselect(int start, int end);
+
+	void deselectAll();
+
+	EditorTableColumn getColumn(int index);
+
+	int getColumnCount();
+
+	int[] getColumnOrder();
+
+	EditorTableColumn[] getColumns();
+
+	int getGridLineWidth();
+
+	int getHeaderHeight();
+
+	boolean getHeaderVisible();
+
+	EditorTableItem getItem(int index);
+
+	EditorTableItem getItem(Point point);
+
+	int getItemCount();
+
+	int getItemHeight();
+
+	EditorTableItem[] getItems();
+
+	boolean getLinesVisible();
+
+	EditorTableItem[] getSelection();
+
+	int getSelectionCount();
+
+	int getSelectionIndex();
+
+	int getSortDirection();
+
+	int getTopIndex();
+
+	int indexOf(EditorTableColumn column);
+
+	int indexOf(EditorTableItem item);
+
+	boolean isSelected(int index);
+
+	void remove(int index);
+
+	void remove(int[] indices);
+
+	void remove(int start, int end);
+
+	void removeAll();
+
+	void select(int index);
+
+	void select(int[] indices);
+
+	void select(int start, int end);
+
+	void selectAll();
+
+	void setColumnOrder(int[] order);
+
+	void setHeaderVisible(boolean show);
+
+	void setItemCount(int count);
+
+	void setLinesVisible(boolean show);
+
+	void setRedraw(boolean redraw);
+
+	void setSelection(int index);
+
+	void setSelection(int[] indices);
+
+	void setSelection(int start, int end);
+
+	void setSelection(EditorTableItem item);
+
+	void setSelection(EditorTableItem[] items);
+
+	void setSortColumn(EditorTableColumn column);
+
+	void setSortDirection(int direction);
+
+	void setTopIndex(int index);
+
+	void showColumn(EditorTableColumn column);
+
+	void showItem(EditorTableItem item);
+
+	void showSelection();
+}
