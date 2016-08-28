0441c71e731ad5f7e00a7945acfdaa6eddd0eff1
Dzn <dzngit@gmail.com>
2016-08-28 22:51:44 +0200

    -

---
+++ core/src/com/gurella/engine/editor/ui/EditorTableItem.java
@@ -0,0 +1,64 @@
+package com.gurella.engine.editor.ui;
+
+import com.badlogic.gdx.graphics.Color;
+import com.gurella.engine.utils.GridRectangle;
+
+public interface EditorTableItem extends EditorItem {
+	Color getBackground();
+
+	Color getBackground(int index);
+
+	GridRectangle getBounds();
+
+	GridRectangle getBounds(int index);
+
+	boolean getChecked();
+
+	FontData getFont();
+
+	FontData getFont(int index);
+
+	Color getForeground();
+
+	Color getForeground(int index);
+
+	boolean getGrayed();
+
+	EditorImage getImage(int index);
+
+	GridRectangle getImageBounds(int index);
+
+	int getImageIndent();
+
+	Table getParent();
+
+	String getText(int index);
+
+	GridRectangle getTextBounds(int index);
+
+	void setBackground(Color color);
+
+	void setBackground(int index, Color color);
+
+	void setChecked(boolean checked);
+
+	void setFont(FontData font);
+
+	void setFont(int index, FontData font);
+
+	void setForeground(Color color);
+
+	void setForeground(int index, Color color);
+
+	void setGrayed(boolean grayed);
+
+	void setImage(EditorImage[] images);
+
+	void setImage(int index, EditorImage image);
+
+	void setImageIndent(int indent);
+
+	void setText(int index, String string);
+
+	void setText(String[] strings);
+}
