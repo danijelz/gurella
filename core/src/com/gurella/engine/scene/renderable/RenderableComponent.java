8c8ff3ea5fad598b8f1dd150fa81779006d99f6e
Dzn <dzngit@gmail.com>
2016-08-28 22:04:37 +0200

    -

--- core/src/com/gurella/engine/scene/renderable/RenderableComponent.java
+++ core/src/com/gurella/engine/scene/renderable/RenderableComponent.java
@@ -155,6 +155,7 @@ public abstract class RenderableComponent extends SceneNodeComponent2
 		private static void createCheck(PropertyEditorContext<Byte> context, EditorComposite parent, String text,
 				byte index) {
 			EditorUiFactory uiFactory = parent.getUiFactory();
+			@SuppressWarnings("unchecked")
 			EditorButton check = uiFactory.createCheckBox(parent, text);
 			byte byteValue = context.getPropertyValue().byteValue();
 			check.setSelection((byteValue & (1 << index)) != 0);
