2203c806e56c0770e855e9ba02f1d2846a81f0a3
Dzn <dzngit@gmail.com>
2016-08-29 20:19:36 +0200

    -

--- core/src/com/gurella/engine/scene/bullet/shapes/Box2dCollisionShape.java
+++ core/src/com/gurella/engine/scene/bullet/shapes/Box2dCollisionShape.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.physics.bullet.collision.btBox2dShape;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
 
 public class Box2dCollisionShape extends BulletCollisionShape {
-	public final Vector3 halfExtents = new Vector3(0.5f, 0.5f, 0);
+	public final Vector3 halfExtents = new Vector3(0.5f, 0.5f, 0.5f);
 
 	@Override
 	public btCollisionShape createNativeShape() {
