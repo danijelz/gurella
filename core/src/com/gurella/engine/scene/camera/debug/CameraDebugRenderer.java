0790e0ebd3b62a99ab490890563be8cab133eeeb
Dzn <dzngit@gmail.com>
2016-10-17 21:19:29 +0200

    -

--- core/src/com/gurella/engine/scene/camera/debug/CameraDebugRenderer.java
+++ core/src/com/gurella/engine/scene/camera/debug/CameraDebugRenderer.java
@@ -3,11 +3,12 @@ package com.gurella.engine.scene.camera.debug;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.DepthTestAttribute;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.utils.Array;
 import com.gurella.engine.graphics.render.GenericBatch;
 import com.gurella.engine.scene.camera.CameraComponent;
@@ -19,7 +20,6 @@ import com.gurella.engine.scene.spatial.SpatialSystem;
 public class CameraDebugRenderer {
 	private CameraComponent<?> cameraComponent;
 	private final FrameBuffer fbo;
-	private final Sprite sprite;
 	private final Array<Spatial> spatials = new Array<Spatial>();
 	private final LayerMask layerMask = new LayerMask();
 
@@ -28,17 +28,23 @@ public class CameraDebugRenderer {
 	private final ColorAttribute fog = new ColorAttribute(ColorAttribute.Fog, 1f, 1f, 1f, 1f);
 	private final DepthTestAttribute depthTest = new DepthTestAttribute();
 
+	private Matrix4 infoProjection;
+	private SpriteBatch spriteBatch;
+
 	public CameraDebugRenderer(CameraComponent<?> cameraComponent) {
 		this.cameraComponent = cameraComponent;
+
 		int width = 240;
 		int height = 180;
+
 		fbo = new FrameBuffer(Format.RGBA8888, width, height, true);
-		sprite = new Sprite(fbo.getColorBufferTexture());
-		sprite.setBounds(100, 300, width, height);
 
 		environment.set(ambientLight);
 		environment.set(fog);
 		environment.set(depthTest);
+
+		infoProjection = new Matrix4().setToOrtho2D(0, 0, width, height);
+		spriteBatch = new SpriteBatch();
 	}
 
 	public void debugRender(GenericBatch batch) {
@@ -52,11 +58,15 @@ public class CameraDebugRenderer {
 		spatialSystem.getSpatials(cameraComponent.camera.frustum, spatials, layerMask);
 		fbo.end();
 
-		batch.render(sprite);
+		spriteBatch.setProjectionMatrix(infoProjection);
+		spriteBatch.begin();
+		spriteBatch.draw(fbo.getColorBufferTexture(), 100, 100);
+		spriteBatch.end();
 		// TODO Auto-generated method stub
 	}
 
 	public void dispose() {
 		fbo.dispose();
+		spriteBatch.dispose();
 	}
 }
