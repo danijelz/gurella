bb62e367949bd6af3f37c94f286dd20db8d6a725
Dzn <dzngit@gmail.com>
2016-10-27 20:53:06 +0200

    -

--- studio/editor/src/com/gurella/studio/editor/tool/ToolManager.java
+++ studio/editor/src/com/gurella/studio/editor/tool/ToolManager.java
@@ -3,5 +3,12 @@ package com.gurella.studio.editor.tool;
 import com.badlogic.gdx.InputAdapter;
 
 public class ToolManager extends InputAdapter {
+	private final int editorId;
+	
 	ScaleTool scaleTool;
+
+	public ToolManager(int editorId) {
+		this.editorId = editorId;
+	}
+	
 }
