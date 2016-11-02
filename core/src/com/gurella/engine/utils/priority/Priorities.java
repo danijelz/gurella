cb38063f6952785edad845c22f96a6e8c3031e46
Dzn <dzngit@gmail.com>
2016-11-02 20:04:05 +0100

    -

--- core/src/com/gurella/engine/utils/priority/Priorities.java
+++ core/src/com/gurella/engine/utils/priority/Priorities.java
@@ -9,93 +9,82 @@ public class Priorities {
 	private static final ObjectIntMap<Class<?>> globalPriorities = new ObjectIntMap<Class<?>>();
 	private static final ObjectMap<Class<?>, ObjectIntMap<Class<?>>> interfacePriorities = new ObjectMap<Class<?>, ObjectIntMap<Class<?>>>();
 
+	static {
+		globalPriorities.put(Object.class, 0);
+	}
+
 	public static int getPriority(Object obj) {
 		return getPriority(obj.getClass());
 	}
 
-	public static int getPriority(Class<?> objType) {
-		if (globalPriorities.containsKey(objType)) {
-			return globalPriorities.get(objType, 0);
+	public static int getPriority(Class<?> type) {
+		if (!globalPriorities.containsKey(type)) {
+			synchronized (globalPriorities) {
+				initPriorities(type);
+			}
 		}
-		return initPriorities(objType, null);
+
+		return globalPriorities.get(type, 0);
 	}
 
 	public static int getPriority(Object obj, Class<?> interfaceType) {
 		return getPriority(obj.getClass(), interfaceType);
 	}
 
-	public static int getPriority(Class<?> objType, Class<?> interfaceType) {
-		ObjectIntMap<Class<?>> prioritiesByEvent = interfacePriorities.get(objType);
-		if (prioritiesByEvent == null) {
-			prioritiesByEvent = new ObjectIntMap<Class<?>>();
-			interfacePriorities.put(objType, prioritiesByEvent);
+	public static int getPriority(Class<?> type, Class<?> interfaceType) {
+		if (!globalPriorities.containsKey(type)) {
+			synchronized (globalPriorities) {
+				initPriorities(type);
+			}
 		}
 
-		if (prioritiesByEvent.containsKey(interfaceType)) {
-			return prioritiesByEvent.get(interfaceType, 0);
-		}
-
-		Class<?> type = interfaceType;
-		while (type != null && type != Object.class) {
-			ObjectIntMap<Class<?>> prioritiesByType = interfacePriorities.get(objType);
-			if (prioritiesByType != null && prioritiesByType.containsKey(type)) {
-				int value = prioritiesByType.get(interfaceType, 0);
-				prioritiesByEvent.put(interfaceType, value);
-				return value;
-			}
-
-			TypePriority typePriority = getAnnotation(type, TypePriority.class);
-			if (typePriority != null && objType.getClass().equals(typePriority.type())) {
-				int value = typePriority.priority();
-				prioritiesByEvent.put(interfaceType, value);
-				return value;
-			}
-
-			TypePriorities typePriorities = getAnnotation(type, TypePriorities.class);
-			if (typePriorities != null) {
-				TypePriority listenerPriority = findListenerPriority(typePriorities, objType);
-				if (listenerPriority != null) {
-					int value = listenerPriority.priority();
-					prioritiesByEvent.put(interfaceType, value);
-					return value;
-				}
-			}
-			type = type.getSuperclass();
-		}
-
-		type = interfaceType;
-		while (type != null && type != Object.class) {
-			Priority priority = getAnnotation(type, Priority.class);
-			if (priority != null) {
-				int value = priority.value();
-				prioritiesByEvent.put(interfaceType, value);
-				globalPriorities.put(objType, value);
-				return value;
-			}
-			type = type.getSuperclass();
-		}
-
-		prioritiesByEvent.put(interfaceType, 0);
-		return 0;
+		ObjectIntMap<Class<?>> priorities = interfacePriorities.get(type);
+		return priorities == null ? 0 : priorities.get(interfaceType, 0);
 	}
 
-	private static TypePriority findListenerPriority(TypePriorities typePriorities, Class<?> eventType) {
-		if (typePriorities == null) {
-			return null;
+	private static void initPriorities(Class<?> type) {
+		if (globalPriorities.containsKey(type)) {
+			return;
 		}
 
-		TypePriority[] values = typePriorities.value();
-		for (int i = 0; i < values.length; i++) {
-			TypePriority typePriority = values[i];
-			if (eventType.getClass().equals(typePriority.type())) {
-				return typePriority;
+		Class<?> superType = type.getSuperclass();
+		initPriorities(superType);
+		copyPriorities(type, superType);
+
+		TypePriorities typePriorities = getAnnotation(type, TypePriorities.class);
+		if (typePriorities != null) {
+			ObjectIntMap<Class<?>> priorities = getInterfacePriorities(type);
+			TypePriority[] values = typePriorities.value();
+			for (int i = 0; i < values.length; i++) {
+				TypePriority typePriority = values[i];
+				priorities.put(typePriority.type(), typePriority.priority());
 			}
 		}
 
-		return null;
+		TypePriority typePriority = getAnnotation(type, TypePriority.class);
+		if (typePriority != null) {
+			ObjectIntMap<Class<?>> priorities = getInterfacePriorities(type);
+			priorities.put(typePriority.type(), typePriority.priority());
+		}
+
+		Priority priority = getAnnotation(type, Priority.class);
+		globalPriorities.put(type, priority == null ? 0 : priority.value());
 	}
 
-	private static int initPriorities(Class<?> objType, Class<?> interfaceType) {
-		return 0;
+	private static ObjectIntMap<Class<?>> getInterfacePriorities(Class<?> type) {
+		ObjectIntMap<Class<?>> priorities = interfacePriorities.get(type);
+		if (priorities == null) {
+			priorities = new ObjectIntMap<Class<?>>();
+			interfacePriorities.put(type, priorities);
+		}
+		return priorities;
+	}
+
+	private static void copyPriorities(Class<?> type, Class<?> superType) {
+		globalPriorities.put(type, globalPriorities.get(superType, 0));
+		ObjectIntMap<Class<?>> priorities = interfacePriorities.get(superType);
+		if (priorities != null) {
+			interfacePriorities.put(type, new ObjectIntMap<Class<?>>(priorities));
+		}
 	}
 }
