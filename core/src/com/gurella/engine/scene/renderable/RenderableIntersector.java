3d2dedbe47a752fb95ad5bbba9be1d062295ffc5
Dzn <dzngit@gmail.com>
2016-11-06 19:35:19 +0100

    -

--- core/src/com/gurella/engine/scene/renderable/RenderableIntersector.java
+++ core/src/com/gurella/engine/scene/renderable/RenderableIntersector.java
@@ -1,6 +1,7 @@
 package com.gurella.engine.scene.renderable;
 
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.collision.Ray;
