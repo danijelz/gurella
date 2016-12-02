6e2ffc3ab1b2aaa4528464b89ba131b95ac482fe
dzn <dzngit@gmail.com>
2016-12-02 16:14:52 +0100

    -

--- studio/editor/src/com/gurella/studio/refractoring/ReferencesSearchRequestor.java
+++ studio/editor/src/com/gurella/studio/refractoring/ReferencesSearchRequestor.java
@@ -17,8 +17,6 @@ final class ReferencesSearchRequestor extends TextSearchRequestor {
 	private final Map<IFile, TextFileChange> changes;
 	private final String replacement;
 	
-	private TextEditGroup textEditGroup;
-
 	ReferencesSearchRequestor(boolean txtFilesHandled, Map<IFile, TextFileChange> changes, String replacement) {
 		this.txtFilesHandled = txtFilesHandled;
 		this.changes = changes;
