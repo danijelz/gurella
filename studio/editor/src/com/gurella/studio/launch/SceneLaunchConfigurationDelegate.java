019461fa2ce849eb2c4869a3dd5c07c0a25722b3
Dzn <dzngit@gmail.com>
2016-12-09 00:51:58 +0100

    -

---
+++ studio/editor/src/com/gurella/studio/launch/SceneLaunchConfigurationDelegate.java
@@ -0,0 +1,16 @@
+package com.gurella.studio.launch;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+
+public class SceneLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
+		// TODO Auto-generated method stub
+		
+	}
+}
