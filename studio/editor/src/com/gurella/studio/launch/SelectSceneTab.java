44f73a950276a6055445dc9e78d0c18ef99ba6c7
Dzn <dzngit@gmail.com>
2016-12-09 01:20:34 +0100

    -

---
+++ studio/editor/src/com/gurella/studio/launch/SelectSceneTab.java
@@ -0,0 +1,38 @@
+package com.gurella.studio.launch;
+
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.swt.widgets.Composite;
+
+public class SelectSceneTab extends AbstractLaunchConfigurationTab {
+
+	@Override
+	public void createControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		return "Main";
+	}
+}
