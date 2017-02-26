2bba83e25a78b259d21799fcb58f33917b02e66e
Dzn <dzngit@gmail.com>
2017-02-26 22:03:53 +0100

    -

--- core/src/com/gurella/engine/utils/struct/StructArray.java
+++ core/src/com/gurella/engine/utils/struct/StructArray.java
@@ -1,7 +1,13 @@
 package com.gurella.engine.utils.struct;
 
+import com.gurella.engine.utils.Reflection;
+
 public class StructArray<T extends Struct> extends Buffer {
+	private final T temp;
+	private int size;
+	
 	public StructArray(StructType<T> structType, int initialSize) {
 		super(structType.size * initialSize);
+		temp = Reflection.newInstance(structType.type);
 	}
 }
