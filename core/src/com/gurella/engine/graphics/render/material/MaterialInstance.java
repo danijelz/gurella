d758ee1a1c4b1fe2793c2cb8fc33cff8a2d5763c
Dzn <dzngit@gmail.com>
2017-03-09 21:21:20 +0100

    -

--- core/src/com/gurella/engine/graphics/render/material/MaterialInstance.java
+++ core/src/com/gurella/engine/graphics/render/material/MaterialInstance.java
@@ -1,10 +1,16 @@
 package com.gurella.engine.graphics.render.material;
 
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.ObjectMap;
 
-public class MaterialInstance {
+public class MaterialInstance implements Disposable {
 	private Material material;
 	private Technique technique;
 	private ObjectMap<Pass, ShaderProgram> passPrograms = new ObjectMap<Pass, ShaderProgram>();
+	
+	@Override
+	public void dispose() {
+		//TODO
+	}
 }
